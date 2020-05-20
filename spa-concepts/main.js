7786d3bea4a7f7daf59c02032779a9e9c59a1cf1
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-20 20:03:26 -0300

    history api método replace state

--- spa-concepts/main.js
+++ spa-concepts/main.js
@@ -1,35 +1,36 @@
 (function () {
-    'use strict'
+  'use strict'
 
-    var $links = document.querySelectorAll('a')
-    var $h1 = document.querySelector('h1')
+  var $links = document.querySelectorAll('a')
+  var $h1 = document.querySelector('h1')
 
-    $links.forEach(function($link){
-        $link.addEventListener('click', handleClick, false)
-    })
+  $links.forEach(function ($link) {
+    $link.addEventListener('click', handleClick, false)
+  })
 
-    window.onpopstate = function(e) {
-        navigation({url: e.state.url})
+  window.onpopstate = function (e) {
+    navigation({ url: e.state.url })
+  }
+
+  history.replaceState({ url: '/' }, '', '/')
+
+  function navigation(state) {
+    var titles = {
+      '/': 'Home',
+      '/sobre': 'Sobre'
     }
+    $h1.textContent = titles[state.url]
+  }
 
-    function navigation (state) {
-        console.log(state)
-        var titles = {
-            '/': 'Home',
-            '/sobre': 'Sobre'
-        }
-        $h1.textContent = titles[state.url]
+  function handleClick(e) {
+    e.preventDefault()
+    var title = ''
+    var url = e.target.getAttribute('href')
+    var state = {
+      url: url
     }
-
-    function handleClick(e) {
-        e.preventDefault()
-        var title = ''
-        var url = e.target.getAttribute('href')
-        var state = {
-            url: url
-        }
-        history.pushState(state, title, url)
-        navigation({url: url})
-    }
+    history.pushState(state, title, url)
+    navigation({ url: url })
+  }
 
 })()
